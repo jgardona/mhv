050738704d8249d3ee8e756c9fa166cd1abff346
jgardona <jcbritobr@gmail.com>
2023-11-23 15:16:36 -0300

    Implements color characters * This commit adds color characters support. * New integration tests added.

--- src/cli.rs
+++ src/cli.rs
@@ -9,11 +9,11 @@ use self::{errors::Result, fs::read_data, view::display_data};
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Skip `n` offset(1 = 16) bytes
+    /// Skip `n` bytes
     #[arg(short, long, default_value_t = 0)]
     skip: usize,
 
-    /// Only read `n` offset bytes from the input. Skip for full read
+    /// Read `n` bytes. None for full read
     #[arg(short, long)]
     length: Option<usize>,
 
--- src/cli/view.rs
+++ src/cli/view.rs
@@ -1,41 +1,44 @@
+use colored::Colorize;
+
 const OFFSET: usize = 16;
 
 pub fn display_data(skip: usize, length: usize, buffer: &[u8]) {
-    let mut position = skip * OFFSET;
-    buffer.chunks(OFFSET).skip(skip).take(length).for_each(|d| {
-        print!("[0x{:08x}] ", position);
-        for b in d {
-            match *b {
-                0x00 => print!(".  "),
-                0xff => print!("## "),
-                _ => print!("{:02x} ", b),
+    let mut position = skip;
+    buffer
+        .iter()
+        .skip(skip)
+        .take(length)
+        .copied()
+        .collect::<Vec<u8>>()
+        .chunks(OFFSET)
+        .for_each(|d| {
+            print!("{}", format!("[0x{:08x}] ", position).bright_black());
+            for b in d {
+                match *b {
+                    // null byte
+                    0x00 | 0xff => print!("{}  ", "•".bright_black()),
+                    // ascii printable characters
+                    0x21..=0x7e => print!("{} ", format!("{:02x}", b).blue()),
+                    // ascii white space characters and controls
+                    0x01..=0x08 | 0x0e..=0x1f => print!("{} ", format!("{:02x}", b).green()),
+                    0x09..=0x0d | 0x20 | 0x7f => print!("{} ", format!("{:02x}", b).green()),
+                    // ascii extended codes
+                    0x80..=0xfe => print!("{} ", format!("{:02x}", b).bright_red()),
+                }
             }
-        }
 
-        print!(" | ");
+            print!("| ");
 
-        for b in d {
-            match *b {
-                0x00..=0x20 | 0x7f..=0xff => print!(". "),
-                _ => print!("{} ", *b as char),
+            for b in d {
+                match *b {
+                    0x00 | 0xff => print!("{}", "•".bright_black()),
+                    0x21..=0x7e => print!("{}", format!("{}", *b as char).blue()),
+                    0x09..=0x0d | 0x20 | 0x7f => print!("{}", "_".green()),
+                    0x01..=0x08 | 0x0e..=0x1f => print!("{}", "•".green()),
+                    0x80..=0xfe => print!("{}", "•".bright_red()),
+                }
             }
-        }
-        println!();
-        position += OFFSET;
-    })
-}
-
-#[cfg(test)]
-mod view_test {
-    use super::display_data;
-
-    #[test]
-    fn test_display_data() {
-        let skip = 0;
-        let length = 10;
-        let data = vec![
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        ];
-        display_data(skip, length, &data);
-    }
+            println!();
+            position += OFFSET;
+        })
 }

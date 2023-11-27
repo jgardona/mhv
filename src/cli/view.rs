c0559965ea96dc1cd13e454f534a36b2d3daef1a
jgardona <jcbritobr@gmail.com>
2023-11-27 19:31:03 -0300

    Ref: Use owo colors Owo colors crate doen't allocate. We got a boost in performance:
     hyperfine --warmup 1000 -N 'target/release/mhv -l1kb tests/data/data1kb' 'mhv -l1kb tests/data/data1kb'
    Benchmark 1: target/release/mhv -l1kb tests/data/data1kb
      Time (mean ± σ):       3.8 ms ±   0.1 ms    [User: 1.3 ms, System: 2.0 ms]
      Range (min … max):     3.6 ms …   4.4 ms    746 runs
    
    Benchmark 2: mhv -l1kb tests/data/data1kb
      Time (mean ± σ):       4.1 ms ±   0.2 ms    [User: 1.6 ms, System: 2.0 ms]
      Range (min … max):     3.9 ms …   4.9 ms    608 runs
    
      Warning: Statistical outliers were detected. Consider re-running this benchmark on a quiet system without any interferences from other programs. It might help to use the '--warmup' or '--prepare' options.
    
    Summary
      target/release/mhv -l1kb tests/data/data1kb ran
        1.08 ± 0.06 times faster than mhv -l1kb tests/data/data1kb

--- src/cli/view.rs
+++ src/cli/view.rs
@@ -1,46 +1,48 @@
-use colored::Colorize;
+use owo_colors::*;
 use std::io::{Cursor, Write};
 
 const OFFSET: usize = 16;
 static EMPTY_LINE: &str = "*";
 
-pub fn display_data(skip: usize, nosqueezing: bool, buffer: &[u8]) -> std::io::Result<()> {
+pub fn display_data<W: Write>(
+    skip: usize,
+    nosqueezing: bool,
+    buffer: &[u8],
+    writer: &mut W,
+) -> std::io::Result<()> {
     let mut position = skip;
     let mut output = Cursor::new(Vec::<u8>::new());
     let mut old_buffer = [0u8; 16];
     let mut is_printing = false;
+    let mut first_line = true;
     buffer.chunks(OFFSET).for_each(|d| {
-        if !squeeze(d, &mut old_buffer, &mut output, nosqueezing, &mut is_printing) {
-            write!(
-                output,
-                "{}",
-                format!("[0x{:08x}] ", position).bright_black()
-            )
-            .unwrap();
+        if !squeeze(
+            d,
+            &mut old_buffer,
+            &mut output,
+            nosqueezing,
+            &mut is_printing,
+            &mut first_line,
+        ) {
+            write!(output, "{:08x} ", position.bright_black()).unwrap();
             for b in d {
                 match *b {
                     // null byte
                     0x00 | 0xff => write!(output, "{}  ", "•".bright_black()).unwrap(),
                     // ascii printable characters
-                    0x21..=0x7e => write!(output, "{} ", format!("{:02x}", b).blue()).unwrap(),
+                    0x21..=0x7e => write!(output, "{:02x} ", b.blue()).unwrap(),
                     // ascii white space characters and controls
-                    0x01..=0x08 | 0x0e..=0x1f => {
-                        write!(output, "{} ", format!("{:02x}", b).green()).unwrap()
-                    }
-                    0x09..=0x0d | 0x20 | 0x7f => {
-                        write!(output, "{} ", format!("{:02x}", b).green()).unwrap()
-                    }
+                    0x01..=0x08 | 0x0e..=0x1f => write!(output, "{:02x} ", b.green()).unwrap(),
+                    0x09..=0x0d | 0x20 | 0x7f => write!(output, "{:02x} ", b.green()).unwrap(),
                     // ascii extended codes
-                    0x80..=0xfe => {
-                        write!(output, "{} ", format!("{:02x}", b).bright_red()).unwrap()
-                    }
+                    0x80..=0xfe => write!(output, "{:02x} ", b.bright_red()).unwrap(),
                 }
             }
 
             for b in d {
                 match *b {
                     0x00 | 0xff => write!(output, "{}", "•".bright_black()).unwrap(),
-                    0x21..=0x7e => write!(output, "{}", format!("{}", *b as char).blue()).unwrap(),
+                    0x21..=0x7e => write!(output, "{}", (*b as char).blue()).unwrap(),
                     0x09..=0x0d | 0x20 | 0x7f => write!(output, "{}", "_".green()).unwrap(),
                     0x01..=0x08 | 0x0e..=0x1f => write!(output, "{}", "•".green()).unwrap(),
                     0x80..=0xfe => write!(output, "{}", "•".bright_red()).unwrap(),
@@ -52,7 +54,7 @@ pub fn display_data(skip: usize, nosqueezing: bool, buffer: &[u8]) -> std::io::R
     });
 
     output.set_position(0);
-    std::io::copy(&mut output, &mut std::io::stdout())?;
+    std::io::copy(&mut output, writer)?;
 
     Ok(())
 }
@@ -63,20 +65,27 @@ fn squeeze<'a, W: Write>(
     writer: &mut W,
     squeezing: bool,
     printed: &mut bool,
+    first_line: &mut bool,
 ) -> bool {
     if new_buffer.len() != old_buffer.len() || !squeezing {
         return false;
     }
     if new_buffer == old_buffer {
+        if *first_line {
+            *first_line = false;
+            return false;
+        }
         if !*printed {
             writeln!(writer, "{}", EMPTY_LINE.bright_black()).expect("cant write to writer");
             *printed = true;
         }
+
         old_buffer.copy_from_slice(new_buffer);
         return true;
     }
 
     *printed = false;
+    *first_line = false;
     old_buffer.copy_from_slice(new_buffer);
 
     false
@@ -84,29 +93,37 @@ fn squeeze<'a, W: Write>(
 
 #[cfg(test)]
 mod test_view {
-    use std::io::Cursor;
+    use std::{io::Cursor, ops::RangeInclusive};
 
-    use super::squeeze;
+    use super::{display_data, squeeze};
+    use anyhow::{Ok, Result};
+
+    const OFFSET: RangeInclusive<usize> = RangeInclusive::new(0, 17);
+    const FIRST_CHAR: RangeInclusive<usize> = RangeInclusive::new(19, 30);
 
     #[test]
     fn test_squeeze() {
-        let new_buffer = vec![
-            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-            1, 1, 1,
-        ];
+        let new_buffer = vec![0u8; 32];
         let mut cursor = Cursor::new(Vec::<u8>::new());
         let mut old_buffer = [0u8; 16];
         let mut is_print = false;
+        let mut first_line = true;
+        const OFFSET: usize = 16;
 
-        new_buffer.chunks(16).for_each(|d| {
-            if squeeze(d, &mut old_buffer, &mut cursor, true, &mut is_print) {
+        new_buffer.chunks(OFFSET).for_each(|d| {
+            if squeeze(
+                d,
+                &mut old_buffer,
+                &mut cursor,
+                true,
+                &mut is_print,
+                &mut first_line,
+            ) {
                 assert_eq!(d, old_buffer);
             }
         });
-        assert_eq!(
-            "\u{1b}[90m*\u{1b}[0m\n",
-            String::from_utf8_lossy(&cursor.into_inner())
-        );
+        let striped_buffer = strip_ansi_escapes::strip(cursor.into_inner());
+        assert_eq!("*\n", String::from_utf8_lossy(&striped_buffer));
     }
 
     #[test]
@@ -115,12 +132,144 @@ mod test_view {
         let mut cursor = Cursor::new(Vec::<u8>::new());
         let mut old_buffer = [0u8; 16];
         let mut is_print = false;
+        let mut first_line = true;
 
         new_buffer.chunks(16).for_each(|d| {
-            if squeeze(d, &mut old_buffer, &mut cursor, true, &mut is_print) {
+            if squeeze(
+                d,
+                &mut old_buffer,
+                &mut cursor,
+                true,
+                &mut is_print,
+                &mut first_line,
+            ) {
                 assert_eq!(d, old_buffer);
             }
         });
         assert_eq!("", String::from_utf8_lossy(&cursor.into_inner()));
+    }
+
+    #[test]
+    fn test_display_data_16_null_bytes() -> Result<()> {
+        let input = [0u8; 16];
+        let expected =
+            "00000000 •  •  •  •  •  •  •  •  •  •  •  •  •  •  •  •  ••••••••••••••••\n";
+        let mut output = Cursor::new(Vec::<u8>::new());
+
+        display_data(0, false, &input, &mut output)?;
+
+        let result = output.get_ref();
+        let plain_result = strip_ansi_escapes::strip(result);
+        assert_eq!(expected, std::str::from_utf8(&plain_result)?);
+
+        let result = String::from_utf8(output.into_inner())?;
+        println!("{result}");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_offset_color_must_be_bright_black() -> Result<()> {
+        let new_buffer = [0u8; 16];
+        let mut output = Cursor::new(Vec::<u8>::new());
+        display_data(0, false, &new_buffer, &mut output)?;
+        let result = output.get_ref();
+        let offset_data = &result[OFFSET];
+        let expect = [
+            0x1b, 0x5b, 0x39, 0x30, 0x6d, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x1b,
+            0x5b, 0x33, 0x39, 0x6d,
+        ];
+        // prove output offset section data is eight zeros with ansi code bright black color
+        assert_eq!(expect, offset_data);
+        Ok(())
+    }
+
+    #[test]
+    fn test_control_character_must_be_green() -> Result<()> {
+        let new_buffer = [0x05; 16];
+        let mut output = Cursor::new(Vec::<u8>::new());
+        display_data(0, false, &new_buffer, &mut output)?;
+        let result = output.get_ref();
+        let first_char = &result[FIRST_CHAR];
+        // ansi code red 0x05 ascii control
+        let expected = [
+            0x1b, 0x5b, 0x33, 0x32, 0x6d, 0x30, 0x35, 0x1b, 0x5b, 0x33, 0x39, 0x6d,
+        ];
+        assert_eq!(expected, first_char);
+        Ok(())
+    }
+
+    #[test]
+    fn test_spaces_character_must_be_green() -> Result<()> {
+        let new_buffer = [0x20; 16];
+        let mut output = Cursor::new(Vec::<u8>::new());
+        display_data(0, false, &new_buffer, &mut output)?;
+        let result = output.get_ref();
+        let first_char = &result[FIRST_CHAR];
+        // ansi code green 0x05 ascii control
+        let expected = [
+            0x1b, 0x5b, 0x33, 0x32, 0x6d, 0x32, 0x30, 0x1b, 0x5b, 0x33, 0x39, 0x6d,
+        ];
+        assert_eq!(expected, first_char);
+        Ok(())
+    }
+
+    #[test]
+    fn test_extended_character_must_be_red() -> Result<()> {
+        let new_buffer = [0x80; 16];
+        let mut output = Cursor::new(Vec::<u8>::new());
+        display_data(0, false, &new_buffer, &mut output)?;
+        let result = output.get_ref();
+        let first_char = &result[FIRST_CHAR];
+        // ansi code red 0x05 ascii extended
+        let expected = [
+            0x1b, 0x5b, 0x39, 0x31, 0x6d, 0x38, 0x30, 0x1b, 0x5b, 0x33, 0x39, 0x6d,
+        ];
+        assert_eq!(expected, first_char);
+        Ok(())
+    }
+
+    #[test]
+    fn test_display_data_squeezing_line_restore() -> Result<()> {
+        let mut new_buffer = vec![0u8; 32];
+        new_buffer.extend(vec![1u8; 16]);
+        let mut output = Cursor::new(Vec::<u8>::new());
+        display_data(0, true, &new_buffer, &mut output)?;
+        let result = output.get_ref();
+        let plain_result = strip_ansi_escapes::strip(result);
+
+        let result = std::str::from_utf8(&plain_result)?;
+
+        let expected =
+            "00000000 •  •  •  •  •  •  •  •  •  •  •  •  •  •  •  •  ••••••••••••••••\n\
+            *\n\
+            00000020 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 ••••••••••••••••\n";
+
+        assert_eq!(expected, result);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_display_data_squeezing_after_first_line_restore() -> Result<()> {
+        let mut new_buffer = vec![1u8; 16];
+        new_buffer.extend(vec![0u8; 32]);
+        new_buffer.extend(vec![1u8; 16]);
+        let mut output = Cursor::new(Vec::<u8>::new());
+        display_data(0, true, &new_buffer, &mut output)?;
+        let result = output.get_ref();
+        let plain_result = strip_ansi_escapes::strip(result);
+
+        let result = std::str::from_utf8(&plain_result)?;
+
+        let expected =
+            "00000000 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 ••••••••••••••••\n\
+            00000010 •  •  •  •  •  •  •  •  •  •  •  •  •  •  •  •  ••••••••••••••••\n\
+            *\n\
+            00000030 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 ••••••••••••••••\n";
+
+        assert_eq!(expected, result);
+
+        Ok(())
     }
 }

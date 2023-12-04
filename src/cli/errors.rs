efdaf4c006e3ce9eb1c1a83fbfdfb876091a1880
jgardona <jcbritobr@gmail.com>
2023-12-04 18:35:54 -0300

    Remove error implementation from main.rs This commit removed error implementation from main.rs.

--- src/cli/errors.rs
+++ src/cli/errors.rs
@@ -1,8 +1,23 @@
+use std::fmt::Display;
+
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 pub static ERR_CANT_PARSE_NUMBER: &str = "Cant parse input number";
 pub static ERR_NOT_AVAILABLE_DATA: &str =
     "Cant read data. Check if you are trying to read more than is available, or if the file exists";
+
+#[derive(Debug)]
+pub struct CliError {
+    pub message: String,
+}
+
+impl std::error::Error for CliError {}
+
+impl Display for CliError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
 
 #[cfg(test)]
 mod errors_test {
